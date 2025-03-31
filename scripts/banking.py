7c6120198a69f2223ec08b1fb2b5c1aae3fbcac0
RedWomanBr <almeidaclara2901@gmail.com>
2025-03-31 15:44:52 -0300

    Update banking.py

--- scripts/banking.py
+++ scripts/banking.py
@@ -1,5 +1,3 @@
-# Este script contém as funcionalidades bancárias do mod TrueBank.
-
 class BankAccount:
     def __init__(self, account_type, balance=0):
         self.account_type = account_type
@@ -16,7 +14,22 @@ class BankAccount:
         else:
             print("Saldo Insuficiente")
 
+    def get_balance(self):
+        return self.balance
+
+
+# Conta Conjunta
+class JointAccount(BankAccount):
+    def __init__(self, account_type, holders, balance=0):
+        super().__init__(account_type, balance)
+        self.holders = holders
+
+
 # Exemplo de uso
 conta = BankAccount("corrente", 100)
 conta.deposit(50)
 conta.withdraw(30)
+
+conta_conjunta = JointAccount("conjunta", ["Sim1", "Sim2"], 200)
+conta_conjunta.deposit(100)
+conta_conjunta.withdraw(50)
