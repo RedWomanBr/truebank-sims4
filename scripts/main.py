3ef6e94db01e779aa2a3418fa396de42bd24988d
RedWomanBr <almeidaclara2901@gmail.com>
2025-03-31 15:34:54 -0300

    Create main.py

---
+++ scripts/main.py
@@ -0,0 +1,8 @@
+# Este é o script principal que inicializa o mod TrueBank.
+
+def main():
+    print("TrueBank Mod Iniciado")
+    # Código para inicializar o mod
+
+if __name__ == "__main__":
+    main()
